ef769da12b386e01b5583ab3206a24adbe39b6d3
sri pavan vemuri <52058483+sripavanv@users.noreply.github.com>
2025-03-02 09:45:20 +0530

    comments

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def process_pdf(file_path):
 # ✅ UI Layout
 app_ui = ui.page_fluid(
     ui.h2("📄 AI-Powered PDF Analyzer"),
-    ui.h("RAG-based LLM. So prompts will make a big difference in retrieval.<br>"
+    ui.h6("RAG-based LLM. So prompts will make a big difference in retrieval.<br>"
       "Consider using Section titles of the document for better results.<br><br>"
       "Do not upload large documents as it will incur heavy costs for me.<br>"
       "Ideally, a technical paper < 80 pages is a good test case.<br><br>"
